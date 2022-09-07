f2c106a76f69606c0fe25e2f31b85444ec94f3de
박수진 <soojin_expo@naver.com>
2022-09-07 14:50:24 +0900

    계단 url 수정

--- mapfront/src/pages/Main.js
+++ mapfront/src/pages/Main.js
@@ -260,7 +260,7 @@ function Main() {
       if(testmap && !markers){
       $.ajax({                //계단 받아옴
         method: "POST",
-        url: "http://localhost:9000/api/find/stair",
+        url: "https://dev.chaerin.shop:9000/api/find/stair",
         async: false,
         data: {
 
