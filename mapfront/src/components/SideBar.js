1e5f3f98ae8c72ad72706738354fe45613044a67
sujin1018 <soojin_expo@naver.com>
2022-09-25 16:28:27 +0900

    추가

--- mapfront/src/components/SideBar.js
+++ mapfront/src/components/SideBar.js
@@ -5,15 +5,18 @@ import traffic from "../images/trafficlight.png";
 import walk from "../images/walkp.png";
 import right from "../images/turnright.png";
 import left from "../images/turnleft.png";
+import point from "../images/placeholder.png";
+// import styled from "styled-components";
 
 
-const SideBar =({width=330, children, totalDistance, totalTime})=>{
+const SideBar =({width=330, children, totalDistance, totalTime, start, end})=>{
     const [isOpen, setOpen] = useState(false);
     const [xPosition, setX] = useState(width);
     const [boxShadow, setBoxShadow] = useState();
     const side = useRef();
     // const [img, setImg] = useState();
 
+    
     const [distance, setDistance] = useState();
     const [time, setTime] = useState();
     var Img;
@@ -40,7 +43,26 @@ const SideBar =({width=330, children, totalDistance, totalTime})=>{
         }
     }
 
+    
+    // const Nav = styled.nav`
+    //     dispaly: flex;
+    //     overflow: auto;
+    //     height: 45px;
+    //     &::-webkit-scrollbar{
+    //         width: 8px;
+    //         height: 8px;
+    //         border-radius: 6px;
+    //         background: rgba(255,255,255,0.4);
+    //     }
+    //     &::-webkit-scrollbar-thumb {
+    //         background: rgba(0,0,0, 0.3);
+    //         border-radius: 6px;
+    //     }
+    //     `;
+
     useEffect(()=>{
+        console.log(start);
+        console.log(end);
         window.addEventListener('click', handleClose);
         return () => {
             window.removeEventListener('click', handleClose);
@@ -76,42 +98,44 @@ const SideBar =({width=330, children, totalDistance, totalTime})=>{
             
             <div ref={side}  className="innersidbar" style={{ width: `${width}px`, height: window.innerHeight,  transform: `translatex(${-xPosition}px)`, backgroundColor: "yellowgreen", boxShadow: boxShadow}}>
                 <button  onClick={() => toggleMenu()}
-                className="sidebarbutton" style={{backgroundColor:"yellowgreen"}}>
+                className="sidebarbutton" style={{backgroundColor:"yellowgreen", fontSize: "smaller"}}>
                 도보 경로
                 </button>
                 <div style={{width: "100%", height: "10%", top: "-70px", textAlign: "left", }}>
-                    <div style={{position: "relative",textAlign: "left", marginLeft: "15px", top: "35%"}}>       
+                    <div style={{top: "-10px"}}>
+                    <text>출발지: {start.name}</text> <br></br>
+                    <i class="bi bi-arrow-down-up"></i> <br></br>
+                    <text>도착지: {end.name}</text>
+                    </div>
+                    <div style={{position: "relative",textAlign: "left", marginLeft: "15px"}}>       
                     <text style={{fontSize: "20px", fontWeight: "1000"}}> {time} </text>
                     <div class="vr" style={{fontWeight: "100"}}></div>
                     <text style={{fontSize: "18px", fontWeight: "600"}}>  {distance}</text>
                     </div>
                 </div>
-                <div className="sidebarcontent" style={{position: "relative", height: "85%", overflowY: "scroll", top: "-70px"}}>
+                <div className="sidebarcontent" style={{position: "relative", height: "85%", overflowY: "scroll", top: "-55px"}}>
                     <div className='list-group' style={{overflowY: "scroll"}}>
                     {children && children.map((obj, index)=>{
                         var Img;
                         if (obj.includes("횡단보도")){
-                            // setImg(traffic);
                             Img=traffic;
                             
                         }
                         else if (obj.includes("우회전")){
-                            // setImg(right);
                             Img=right;
                         }
                         else if (obj.includes("좌회전")){
-                            // setImg(left);
                             Img=left;
                         }
-                        else{
-                            // setImg(walk);
-                            Img=walk; 
+                        else if (obj.includes("도착")){
+                            Img=point;
                         }
-
-                        
+                        else{
+                            Img=walk; 
+                        }               
                         return(
                             <li className='list-group-item'>
-                                <img src={Img} style={{width:"30px", height: "30px" }}/>{obj}
+                                <img src={Img} style={{float:"left",width:"23px", height: "25px", marginRight:"10px"}}/>{obj}
                             </li>
                             
                         );
